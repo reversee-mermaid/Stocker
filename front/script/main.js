c791a9c4dbc7e58450a51c2927b2e02b2712df7b
reversee-mermaid <reversee.mermaid@gmail.com>
2021-01-04 19:47:45 +0900

    Add modal events & styles

--- front/script/main.js
+++ front/script/main.js
@@ -1,12 +1,23 @@
 function setModal(obj) {
-    const modal_inner = document.getElementById('modal');
+    const modal_inner = document.getElementById('modal_inner');
     modal_inner.innerHTML = `
         <img src="${obj.file_path}" alt="Image - ${obj.title}">
         <h2>${obj.title}</h2>
         <strong>${obj.author_name}</strong>
         <p>${obj.description}</p>
         <small>${obj.regdate}</small>
+        <a href="${obj.file_path}" download>📥Download Image</a>
     `;
+}
+
+function hideModal(e) {
+    if(e.target == modal) {
+        modal.classList.remove('show')
+    }
+}
+
+function showModal() {
+    modal.classList.add('show');
 }
 
 function createEl(obj) {
@@ -20,20 +31,25 @@ function createEl(obj) {
         </div>
     `;
 
-    el.onclick = function() {setModal(obj)}
+    el.onclick = function() {
+        setModal(obj);
+        showModal();
+    }
 
     return el;
 }
 
 function init() {
     const container = document.getElementById('container');
-
     axios.get('./data.json')
-        .then(function(res) {
-            res.data.forEach(function(obj) {
-                container.append(createEl(obj));
-            })
+    .then(function(res) {
+        res.data.forEach(function(obj) {
+            container.append(createEl(obj));
         })
+    })
+
+    const modal = document.getElementById('modal');
+    modal.onclick = hideModal;
 }
 
 init();
