ff554cdb959bf940fc80c8e082cccb45de3d339c
reversee-mermaid <reversee.mermaid@gmail.com>
2021-01-30 16:26:03 +0900

    Update 't_user' table

--- query/[create] t_user.sql
+++ query/[create] t_user.sql
@@ -1,5 +1,8 @@
 CREATE TABLE t_user (
 	id int unsigned auto_increment,
 	nm varchar(20) not null,
+	email varchar(50) not null unique,
+	pw varchar(100) not null,
+	salt varchar(50) not null,
 	primary key(id)
 );
