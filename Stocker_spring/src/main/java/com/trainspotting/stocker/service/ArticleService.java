86003d9b4ac0d4877b8fb22ee5918b040d59e7a4
reversee-mermaid <reversee.mermaid@gmail.com>
2021-02-06 22:29:34 +0900

    Add upload function for article service

---
+++ Stocker_spring/src/main/java/com/trainspotting/stocker/service/ArticleService.java
@@ -0,0 +1,52 @@
+package com.trainspotting.stocker.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.trainspotting.stocker.mapper.ArticleMapper;
+import com.trainspotting.stocker.model.*;
+import com.trainspotting.stocker.util.FileUtil;
+
+@Service
+public class ArticleService {
+	
+	@Autowired
+	private FileUtil util;
+	
+	@Autowired
+	private ArticleMapper mapper;
+	
+	public int upload(ArticleDto param, HttpSession session) {	
+		
+		User current_user = (User) session.getAttribute("current_user");
+		
+		try {
+			// file upload
+			String savePath = util.getSavePath(session, current_user.getId());
+			String fileName = util.generateFileName(param.getFile(), param.getTitle());
+			util.save(param.getFile(), savePath, fileName);
+
+			// insert into t_article
+			param.setAuthor_id(current_user.getId());
+			param.setFile_nm(fileName);
+			mapper.insert(param);
+			
+			// insert t_article_tag
+			List<ArticleTag> list = new ArrayList<>();
+			for(int tag_id : param.getTags()) {
+				list.add(new ArticleTag(param.getId(), tag_id));
+			}
+			mapper.insertArticleTag(list);
+			
+			return 1;
+			
+		} catch (Exception e) {}
+		
+		return 0;
+	};
+}
